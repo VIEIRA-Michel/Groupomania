39e1841895038a28b7b1b7c507a34e743ec62de7
Michel Vieira <michelvieira.pro@gmail.com>
2022-06-24 02:19:10 +0200

    Ajout des routes ajouter un commentaire et supprimer un commentaire

--- backend/controllers/publications.js
+++ backend/controllers/publications.js
@@ -56,7 +56,7 @@ exports.createPublication = (req, res, next) => {
 }
 
 exports.updatePublication = (req, res, next) => {
-    const publicationObject = {...req.body}
+    const publicationObject = { ...req.body }
     console.log('publicationObject', publicationObject);
     let now = new Date();
     let today = date.format(now, 'YYYY-MM-DD HH:mm:ss');
@@ -89,6 +89,46 @@ exports.deletePublication = (req, res, next) => {
             } else {
                 console.log(results);
                 res.status(200).json({ message: 'Publication supprimée ! ' })
+            }
+        }
+    )
+}
+
+exports.createComment = (req, res, next) => {
+    let now = new Date();
+    let today = date.format(now, 'YYYY-MM-DD HH:mm:ss');
+    let comment = {
+        user_id: req.user.userId,
+        publication_id: req.params.id,
+        content: req.body.content,
+        created_at: today
+    };
+    let sql = `INSERT INTO comments (user_id, publication_id, content, created_at) VALUES (?, ?, ?, ?);`;
+    connection.query(
+        sql, [comment.user_id, comment.publication_id, comment.content, comment.created_at], function (err, results) {
+            if (err) {
+                console.log(err)
+                res.status(500).json({ message: 'Erreur lors de la création du commentaire' });
+            }
+            if (!err) {
+                console.log('le resultat', results);
+                res.status(201).json({ message: 'Commentaire créé ! ' })
+            }
+
+        }
+    )
+}
+
+exports.deleteComment = (req, res, next) => {
+    let sql = `DELETE FROM comments WHERE id = ?;`;
+    connection.query(
+        sql, [req.params.id], function (err, results) {
+            if (err) {
+                console.log(err)
+                res.status(500).json({ message: 'Erreur lors de la suppression du commentaire' });
+            } else {
+                console.log(results);
+                res.status(200).json({ message: 'Commentaire supprimé ! ' })
             }
         }
     )
--- backend/routes/publications.routes.js
+++ backend/routes/publications.routes.js
@@ -4,9 +4,11 @@ const publicationsController = require('../controllers/publications')
 
 
 router.post('/publications', publicationsController.createPublication);
-// router.get('/publications', publicationsController.getAllPublications);
-router.get('/:id/publications', publicationsController.getPublicationsOfOnePerson);
 router.put('/publications/:id', publicationsController.updatePublication);
 router.delete('/publications/:id', publicationsController.deletePublication);
+router.post('/publications/:id/comments', publicationsController.createComment);
+router.delete('/publications/:id/comments/:id', publicationsController.deleteComment);
+// router.get('/publications', publicationsController.getAllPublications);
+router.get('/:id/publications', publicationsController.getPublicationsOfOnePerson);
 
 module.exports = router;
