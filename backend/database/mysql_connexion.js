b63f8b886994144a8a8cfc757099ad9b9b3fd509
Michel Vieira <80683541+VIEIRA-Michel@users.noreply.github.com>
2022-12-03 17:09:16 +0100

    Update mysql_connexion.js

--- backend/database/mysql_connexion.js
+++ backend/database/mysql_connexion.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   database: 'groupomania',
-  password: '$Pyth0n$'
+  password: ''
 });
 
 module.exports = connection;
